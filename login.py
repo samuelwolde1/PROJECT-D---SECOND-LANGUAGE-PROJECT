2aba0072768b634c57f88e03101b1a404e4648bb
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2024-04-18 11:12:56 -0600

    Add files via upload

--- login.py
+++ login.py
@@ -5,11 +5,8 @@ def signup():
     password = input('Enter password: ')
     confirm_password = input('Confirm password: ')
     if confirm_password == password:
-        enc = confirm_password.encode()
-        hash1 = hashlib.md5(enc).hexdigest()
-        with open('credentials.txt', 'w') as file:
-            file.write(username + '\n') 
-            file.write(hash1)
+        with open('credentials.txt', 'a+') as file:
+            file.write(username + ',' + confirm_password + '\n')
             file.close
             print('You registsered')
     else:
@@ -18,12 +15,10 @@ def signup():
 def login():
      username = input('Enter username: ')
      password = input('Enter password: ')
-     auth = password.encode()
-     auth_hash = hashlib.md5(auth).hexdigest()
      with open('credentials.txt', 'r') as file:
-         stored_username, stored_password = file.read().split('\n')
+         stored_username, stored_password = file.read().split(',')
      file.close()
-     if username == stored_username and auth_hash == stored_password:
+     if username == stored_username and password == stored_password:
          print('Logged in Successfully!')
      else:
          print('Login failed! \n')
